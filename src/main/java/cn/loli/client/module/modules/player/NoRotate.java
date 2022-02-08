9a9a1e79dec5e63270d997fedbff8a53ef398260
诗音澄鸢 镜苑芳依 <dianwangwin@163.com>
2022-02-08 17:08:03 +0800

    Fix NoRotate

--- src/main/java/cn/loli/client/module/modules/player/NoRotate.java
+++ src/main/java/cn/loli/client/module/modules/player/NoRotate.java
@@ -4,6 +4,7 @@ import cn.loli.client.events.PacketEvent;
 import cn.loli.client.module.Module;
 import cn.loli.client.module.ModuleCategory;
 import com.darkmagician6.eventapi.EventTarget;
+import net.minecraft.network.play.client.C03PacketPlayer;
 import net.minecraft.network.play.server.S08PacketPlayerPosLook;
 
 public class NoRotate extends Module {
@@ -17,6 +18,8 @@ public class NoRotate extends Module {
         if (event.getPacket() instanceof S08PacketPlayerPosLook) {
             if (mc.thePlayer != null && mc.theWorld != null && mc.thePlayer.rotationYaw != -180.0f && mc.thePlayer.rotationPitch != 0.0f) {
                 mc.thePlayer.setPosition(((S08PacketPlayerPosLook) event.getPacket()).getX(), ((S08PacketPlayerPosLook) event.getPacket()).getY(), ((S08PacketPlayerPosLook) event.getPacket()).getZ());
+                mc.getNetHandler().getNetworkManager().sendPacket(new C03PacketPlayer.C06PacketPlayerPosLook(((S08PacketPlayerPosLook) event.getPacket()).getX(), ((S08PacketPlayerPosLook) event.getPacket()).getY(),
+                        ((S08PacketPlayerPosLook) event.getPacket()).getZ(), ((S08PacketPlayerPosLook) event.getPacket()).getYaw(), ((S08PacketPlayerPosLook) event.getPacket()).getPitch(), false));
                 event.setCancelled(true);
             }
         }
