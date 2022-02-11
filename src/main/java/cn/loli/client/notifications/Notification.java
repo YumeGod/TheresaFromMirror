e0006d92cc50f4f2cbd80d3149e4a520f20505f3
SuperSkidder <SuperSkidder@outlook.com>
2022-02-11 19:55:14 +0800

    修改Notification

--- src/main/java/cn/loli/client/notifications/Notification.java
+++ src/main/java/cn/loli/client/notifications/Notification.java
@@ -3,6 +3,7 @@
 package cn.loli.client.notifications;
 
 import cn.loli.client.utils.AnimationUtils;
+import cn.loli.client.utils.RenderUtils;
 import cn.loli.client.utils.fontRenderer.GlyphPageFontRenderer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
@@ -11,9 +12,11 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 
 import java.awt.*;
+import java.util.Locale;
 
 public class Notification {
     private static final Minecraft mc = Minecraft.getMinecraft();
@@ -29,6 +32,7 @@ public class Notification {
     private final long end;
     private final AnimationUtils animationUtils = new AnimationUtils();
 
+
     public Notification(NotificationType type, String title, String messsage, int length) {
         this.type = type;
         this.title = title;
@@ -37,72 +41,6 @@ public class Notification {
         fadedIn = 200L * length;
         fadeOut = fadedIn + 500L * length;
         end = fadeOut + fadedIn;
-    }
-
-    public static void drawRect(double left, double top, double right, double bottom, int color) {
-        if (left < right) {
-            double i = left;
-            left = right;
-            right = i;
-        }
-
-        if (top < bottom) {
-            double j = top;
-            top = bottom;
-            bottom = j;
-        }
-
-        float f3 = (float) (color >> 24 & 255) / 255.0F;
-        float f = (float) (color >> 16 & 255) / 255.0F;
-        float f1 = (float) (color >> 8 & 255) / 255.0F;
-        float f2 = (float) (color & 255) / 255.0F;
-        Tessellator tessellator = Tessellator.getInstance();
-        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
-        GlStateManager.enableBlend();
-        GlStateManager.disableTexture2D();
-        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-        GlStateManager.color(f, f1, f2, f3);
-        worldrenderer.begin(7, DefaultVertexFormats.POSITION);
-        worldrenderer.pos(left, bottom, 0.0D).endVertex();
-        worldrenderer.pos(right, bottom, 0.0D).endVertex();
-        worldrenderer.pos(right, top, 0.0D).endVertex();
-        worldrenderer.pos(left, top, 0.0D).endVertex();
-        tessellator.draw();
-        GlStateManager.enableTexture2D();
-        GlStateManager.disableBlend();
-    }
-
-    public static void drawRect(int mode, double left, double top, double right, double bottom, int color) {
-        if (left < right) {
-            double i = left;
-            left = right;
-            right = i;
-        }
-
-        if (top < bottom) {
-            double j = top;
-            top = bottom;
-            bottom = j;
-        }
-
-        float f3 = (float) (color >> 24 & 255) / 255.0F;
-        float f = (float) (color >> 16 & 255) / 255.0F;
-        float f1 = (float) (color >> 8 & 255) / 255.0F;
-        float f2 = (float) (color & 255) / 255.0F;
-        Tessellator tessellator = Tessellator.getInstance();
-        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
-        GlStateManager.enableBlend();
-        GlStateManager.disableTexture2D();
-        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-        GlStateManager.color(f, f1, f2, f3);
-        worldrenderer.begin(mode, DefaultVertexFormats.POSITION);
-        worldrenderer.pos(left, bottom, 0.0D).endVertex();
-        worldrenderer.pos(right, bottom, 0.0D).endVertex();
-        worldrenderer.pos(right, top, 0.0D).endVertex();
-        worldrenderer.pos(left, top, 0.0D).endVertex();
-        tessellator.draw();
-        GlStateManager.enableTexture2D();
-        GlStateManager.disableBlend();
     }
 
     public void show() {
@@ -124,7 +62,7 @@ public class Notification {
     public void render(short number) {
         ScaledResolution res = new ScaledResolution(mc);
         double offset;
-        int width = Math.max(Minecraft.getMinecraft().fontRendererObj.getStringWidth(messsage) + 20, Minecraft.getMinecraft().fontRendererObj.getStringWidth(title) * 2 + 20);
+        int width = Math.max(Minecraft.getMinecraft().fontRendererObj.getStringWidth(messsage) + 35, Minecraft.getMinecraft().fontRendererObj.getStringWidth(title) * 2 + 35);
         int height = 25;
         long time = getTime();
 
@@ -136,17 +74,16 @@ public class Notification {
             offset = width;
         }
 
-        Color backgroundColor = new Color(0, 0, 0, 220);
+        Color backgroundColor = new Color(255, 255, 255, 255);
         Color ribbonColor;
 
         if (type == NotificationType.INFO)
-            ribbonColor = new Color(100, 50, 250);
+            ribbonColor = new Color(68, 119, 255);
         else if (type == NotificationType.WARNING)
             ribbonColor = new Color(250, 210, 0);
         else {
-            ribbonColor = new Color(200, 10, 10);
-            int i = Math.max(0, Math.min(255, (int) (Math.sin(time / 100.0) * 255.0 / 2 + 127.5)));
-            backgroundColor = new Color(i, 0, 0, 220);
+            ribbonColor = new Color(255, 104, 104);
+//            int i = Math.max(0, Math.min(255, (int) (Math.sin(time / 100.0) * 255.0 / 2 + 127.5)));
         }
 
         FontRenderer fontRenderer = Minecraft.getMinecraft().fontRendererObj;
@@ -154,10 +91,11 @@ public class Notification {
         localHeightOffset = animationUtils.animate(heightOffset, localHeightOffset, 0.2, 10);
         heightOffset = localHeightOffset;
 
-        drawRect(res.getScaledWidth() - offset, res.getScaledHeight() - 5 - height - heightOffset, res.getScaledWidth(), res.getScaledHeight() - 5 - heightOffset, backgroundColor.getRGB());
-        drawRect(res.getScaledWidth() - offset, res.getScaledHeight() - 5 - height - heightOffset, res.getScaledWidth() - offset + 4, res.getScaledHeight() - 5 - heightOffset, ribbonColor.getRGB());
+        RenderUtils.drawRoundRect(res.getScaledWidth() - offset, res.getScaledHeight() - 5 - height - heightOffset, res.getScaledWidth() + 4, res.getScaledHeight() - 5 - heightOffset, 3, backgroundColor.getRGB());
+        RenderUtils.drawRoundRect(res.getScaledWidth() - offset, res.getScaledHeight() - height - heightOffset, res.getScaledWidth() - offset + 2, res.getScaledHeight() - 10 - heightOffset, 2, ribbonColor.getRGB());
 
-        fontRenderer.drawString(title, ((int) (res.getScaledWidth() - offset + 8)), (int) ((res.getScaledHeight() - height - heightOffset)) - 3, -1);
-        fontRenderer.drawString(messsage, (int) (res.getScaledWidth() - offset + 8), (int) (res.getScaledHeight() - 16 - heightOffset), -1);
+//        fontRenderer.drawString(title, ((int) (res.getScaledWidth() - offset + 8)), (int) ((res.getScaledHeight() - height - heightOffset)) - 3, -1);
+        RenderUtils.drawImage(new ResourceLocation("theresa/gui/info.png"), (int) (res.getScaledWidth() - offset) + 3, (int) (res.getScaledHeight() - height - heightOffset), 12, 12, 255);
+        fontRenderer.drawString(messsage, (int) (res.getScaledWidth() - offset + 22), (int) (res.getScaledHeight() - height - heightOffset) + 3, new Color(0, 0, 0).getRGB());
     }
 }
--- src/main/java/cn/loli/client/utils/RenderUtils.java
+++ src/main/java/cn/loli/client/utils/RenderUtils.java
@@ -1,15 +1,22 @@
 package cn.loli.client.utils;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.Cylinder;
 
 import java.awt.*;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.GL_BLEND;
 
 public class RenderUtils {
 
@@ -225,4 +232,88 @@ public class RenderUtils {
         float f3 = (float) (color & 255) / 255.0f;
         GL11.glColor4f(f1, f2, f3, f);
     }
+
+
+    public static void drawRoundRect(double xPosition, double yPosition, double endX, double endY, int radius,
+                                     int color) {
+        double width = endX - xPosition;
+        double height = endY - yPosition;
+        Gui.drawRect((int) xPosition + radius, (int) yPosition + radius, (int) (xPosition + width - radius), (int) (yPosition + height - radius),
+                color);
+        Gui.drawRect((int) xPosition, (int) yPosition + radius, (int) xPosition + radius, (int) (yPosition + height - radius), color);
+        Gui.drawRect((int) (xPosition + width - radius), (int) yPosition + radius, (int) (xPosition + width), (int) (yPosition + height - radius),
+                color);
+        Gui.drawRect((int) xPosition + radius, (int) yPosition, (int) (xPosition + width - radius), (int) yPosition + radius, color);
+        Gui.drawRect((int) xPosition + radius, (int) (yPosition + height - radius), (int) (xPosition + width - radius), (int) (yPosition + height),
+                color);
+        drawFilledCircle(xPosition + radius, yPosition + radius, radius, color, 1);
+        drawFilledCircle(xPosition + radius, (int) (yPosition + height - radius), radius, color, 2);
+        drawFilledCircle((int) (xPosition + width - radius), yPosition + radius, radius, color, 3);
+        drawFilledCircle((int) (xPosition + width - radius), (int) (yPosition + height - radius), radius, color, 4);
+        Gui.drawRect(0, 0, 0, 0, 0);
+    }
+
+    public static void drawFilledCircle(double x, double y, double r, int c, int id) {
+        float f = (float) (c >> 24 & 0xff) / 255F;
+        float f1 = (float) (c >> 16 & 0xff) / 255F;
+        float f2 = (float) (c >> 8 & 0xff) / 255F;
+        float f3 = (float) (c & 0xff) / 255F;
+        glEnable(GL_BLEND);
+        glDisable(GL_TEXTURE_2D);
+        glColor4f(f1, f2, f3, f);
+        glBegin(GL_POLYGON);
+        if (id == 1) {
+            glVertex2d(x, y);
+            for (int i = 0; i <= 90; i++) {
+                double x2 = Math.sin((i * Math.PI / 180)) * r;
+                double y2 = Math.cos((i * Math.PI / 180)) * r;
+                glVertex2d(x - x2, y - y2);
+            }
+        } else if (id == 2) {
+            glVertex2d(x, y);
+            for (int i = 90; i <= 180; i++) {
+                double x2 = Math.sin((i * Math.PI / 180)) * r;
+                double y2 = Math.cos((i * Math.PI / 180)) * r;
+                glVertex2d(x - x2, y - y2);
+            }
+        } else if (id == 3) {
+            glVertex2d(x, y);
+            for (int i = 270; i <= 360; i++) {
+                double x2 = Math.sin((i * Math.PI / 180)) * r;
+                double y2 = Math.cos((i * Math.PI / 180)) * r;
+                glVertex2d(x - x2, y - y2);
+            }
+        } else if (id == 4) {
+            glVertex2d(x, y);
+            for (int i = 180; i <= 270; i++) {
+                double x2 = Math.sin((i * Math.PI / 180)) * r;
+                double y2 = Math.cos((i * Math.PI / 180)) * r;
+                glVertex2d(x - x2, y - y2);
+            }
+        } else {
+            for (int i = 0; i <= 360; i++) {
+                double x2 = Math.sin((i * Math.PI / 180)) * r;
+                double y2 = Math.cos((i * Math.PI / 180)) * r;
+                glVertex2f((float) (x - x2), (float) (y - y2));
+            }
+        }
+        glEnd();
+        glEnable(GL_TEXTURE_2D);
+        glDisable(GL_BLEND);
+        Gui.drawRect(0, 0, 0, 0, 0);
+    }
+
+    public static void drawImage(ResourceLocation image, int x, int y, int width, int height, float alpha) {
+        GL11.glDisable(2929);
+        GL11.glEnable(3042);
+        GL11.glDepthMask(false);
+        OpenGlHelper.glBlendFunc(770, 771, 1, 0);
+        GL11.glColor4f(1.0f, 1.0f, 1.0f, alpha);
+        Minecraft.getMinecraft().getTextureManager().bindTexture(image);
+        Gui.drawModalRectWithCustomSizedTexture(x, y, 0.0f, 0.0f, width, height, (float) width, (float) height);
+        GL11.glDepthMask(true);
+        GL11.glDisable(3042);
+        GL11.glEnable(2929);
+    }
+
 }
