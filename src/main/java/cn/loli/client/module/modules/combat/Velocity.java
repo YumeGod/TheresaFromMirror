138af99cb62748383b918288505ecd3ae587f68e
belris2u <bilibri@protonmail.com>
2022-02-15 04:42:55 +0800

    add Velocity reverseHorizon and kbAlert features.(nearby check on the way)

--- src/main/java/cn/loli/client/module/modules/combat/Velocity.java
+++ src/main/java/cn/loli/client/module/modules/combat/Velocity.java
@@ -8,18 +8,18 @@ import cn.loli.client.module.ModuleCategory;
 import cn.loli.client.utils.ChatUtils;
 import cn.loli.client.utils.TimeHelper;
 import cn.loli.client.value.BooleanValue;
-import cn.loli.client.value.ModeValue;
 import cn.loli.client.value.NumberValue;
 import com.darkmagician6.eventapi.EventTarget;
 import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.network.INetHandler;
 import net.minecraft.network.Packet;
-import net.minecraft.network.play.server.*;
+import net.minecraft.network.play.server.S12PacketEntityVelocity;
+import net.minecraft.network.play.server.S27PacketExplosion;
 
-import java.util.ArrayList;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
+// TODO: Nearby Entity Check (this idea skid from "ExHiBitIoN" <- best client ngl)
 public class Velocity extends Module {
 
     private final Queue<TimestampedPacket> packets = new ConcurrentLinkedDeque<>();
@@ -29,6 +29,8 @@ public class Velocity extends Module {
     private final BooleanValue explosion = new BooleanValue("Explosion", true);
     private final BooleanValue legit = new BooleanValue("Jump", false);
     private final BooleanValue choke = new BooleanValue("Choke", false);
+    private final BooleanValue reverseHorizon = new BooleanValue("Reverse Horizon", false);
+    private final BooleanValue kbAlert = new BooleanValue("Alert", false);
     private final NumberValue<Integer> delay = new NumberValue<>("Choke Delay", 400, 0, 800);
     public final BooleanValue antifall = new BooleanValue("AntiFall", false);
     private final NumberValue<Integer> limit = new NumberValue<>("Choke Limit", 8000, 0, 25000);
@@ -54,6 +56,7 @@ public class Velocity extends Module {
 
     @EventTarget
     private void onUpdate(UpdateEvent e) {
+//        ChatUtils.info("Hurttime: " + mc.thePlayer.hurtResistantTime + " | " + mc.thePlayer.velocityChanged);
         if (legit.getObject()) {
             if (mc.thePlayer.hurtTime == 10 && mc.thePlayer.onGround) {
                 mc.thePlayer.jump();
@@ -66,6 +69,8 @@ public class Velocity extends Module {
     private void onPacket(PacketEvent event) {
         float hor = horizon.getObject() / 100f;
         float ver = vertical.getObject() / 100f;
+
+        if (reverseHorizon.getObject()) hor = -hor;
 
         if (legit.getObject())
             return;
@@ -85,32 +90,36 @@ public class Velocity extends Module {
                 ChatUtils.info("Giga " + (Math.abs(packet.func_149149_c()) + Math.abs(packet.func_149144_d()) + Math.abs(packet.func_149147_e())) * 8000);
 
             if (!event.isCancelled()) {
-                if (choke.getObject() && (Math.abs(packet.func_149149_c()) + Math.abs(packet.func_149144_d()) + Math.abs(packet.func_149147_e())) * 8000 < limit.getObject()) {
+                if (choke.getObject() && (packet.func_149149_c() + Math.abs(packet.func_149144_d()) + packet.func_149147_e()) * 8000 < limit.getObject()) {
                     event.setCancelled(true);
                     addPackets(new TimestampedPacket(event.getPacket(), System.currentTimeMillis()), event);
                 }
             }
+
+            if (kbAlert.getObject())
+                kbAlert(event);
         }
 
         if (event.getPacket() instanceof S12PacketEntityVelocity) {
             S12PacketEntityVelocity packet = (S12PacketEntityVelocity) event.getPacket();
             if (packet.getEntityID() == mc.thePlayer.getEntityId()) {
                 // ChatUtils.send("Received A KB Packet");
-                event.setCancelled(true);
 
-                if (horizon.getObject() == 0 && vertical.getObject() == 0)
-                    return;
-
+                if (horizon.getObject() == 0 && vertical.getObject() == 0) {
+                    event.setCancelled(true);
+                }
 
                 if (debug.getObject())
                     ChatUtils.info(String.valueOf(Math.abs(packet.getMotionX()) + Math.abs(packet.getMotionZ()) + Math.abs(packet.getMotionY())));
 
                 if (choke.getObject() &&
-                        Math.abs(packet.getMotionX()) + Math.abs(packet.getMotionZ()) + Math.abs(packet.getMotionY()) < limit.getObject())
+                        Math.abs(packet.getMotionX()) + Math.abs(packet.getMotionZ()) + Math.abs(packet.getMotionY()) < limit.getObject()) {
                     addPackets(new TimestampedPacket(event.getPacket(), System.currentTimeMillis()), event);
-                else
-                    mc.thePlayer.setVelocity(packet.getMotionX() / 8000d * hor, packet.getMotionY() / 8000d * ver, packet.getMotionZ() / 8000d * hor);
+                    event.setCancelled(true);
+                }
 
+                if (kbAlert.getObject())
+                    kbAlert(event);
             }
         }
     }
@@ -153,6 +162,36 @@ public class Velocity extends Module {
         public TimestampedPacket(final Packet<INetHandler> packet, final long timestamp) {
             this.packet = packet;
             this.timestamp = timestamp;
+        }
+    }
+
+    // KB Alert by belris2u
+    private void kbAlert(PacketEvent e) {
+        if (mc.thePlayer.hurtTime == 0 && mc.thePlayer.ticksExisted > 60) {
+            // 收到击退包的tick 如果没有收到伤害 新建线程延迟1tick继续检测
+            new Thread(() -> {
+                try {
+                    Thread.sleep(250);  // After 1 tick
+                    if (mc.thePlayer.hurtTime == 0) {   // 如果还是没有收到任何伤害
+                        if (e.getPacket() instanceof S12PacketEntityVelocity) { // handle S12
+                            S12PacketEntityVelocity packet = (S12PacketEntityVelocity) e.getPacket();
+                            ChatUtils.error("You may have been KB checked! S12 #" + mc.thePlayer.ticksExisted);
+                            // 如果包被cancel就补充一个 不要把所有包都cancel掉然后用setVelocity, 这样我获取不到状态
+                            if (e.isCancelled()) {
+                                mc.thePlayer.setVelocity((double) packet.getMotionX() / 8000.0D, (double) packet.getMotionY() / 8000.0D, (double) packet.getMotionZ() / 8000.0D);
+                            }
+                        } else { // handle S27
+                            S27PacketExplosion packet = (S27PacketExplosion) e.getPacket();
+                            ChatUtils.info("You may have been KB checked! S27 #" + mc.thePlayer.ticksExisted);
+                            if (e.isCancelled()) {
+                                mc.thePlayer.setVelocity(packet.getX(), packet.getY(), packet.getZ());
+                            }
+                        }
+                    }
+                } catch (InterruptedException exception) {
+                    exception.printStackTrace();
+                }
+            }).start();
         }
     }
 }
