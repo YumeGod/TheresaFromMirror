1d3ab35b440c6cf3d3d7fead63bacda02d7cc847
诗音澄鸢 镜苑芳依 <dianwangwin@163.com>
2022-03-01 04:15:10 +0800

    NoFall --> Slient

--- src/main/java/cn/loli/client/module/modules/player/NoFall.java
+++ src/main/java/cn/loli/client/module/modules/player/NoFall.java
@@ -3,36 +3,40 @@
 package cn.loli.client.module.modules.player;
 
 import cn.loli.client.events.MotionUpdateEvent;
-import cn.loli.client.injection.mixins.IAccessorEntityPlayer;
+import cn.loli.client.events.PacketEvent;
 import cn.loli.client.injection.mixins.IAccessorEntityPlayerSP;
+import cn.loli.client.module.Module;
+import cn.loli.client.module.ModuleCategory;
 import cn.loli.client.notifications.Notification;
 import cn.loli.client.notifications.NotificationManager;
 import cn.loli.client.notifications.NotificationType;
-import cn.loli.client.events.UpdateEvent;
-import cn.loli.client.module.Module;
-import cn.loli.client.module.ModuleCategory;
+import cn.loli.client.utils.misc.ChatUtils;
+import cn.loli.client.value.BooleanValue;
 import cn.loli.client.value.ModeValue;
 import com.darkmagician6.eventapi.EventTarget;
 import com.darkmagician6.eventapi.types.EventType;
 import net.minecraft.network.play.client.C03PacketPlayer;
+import net.minecraft.util.AxisAlignedBB;
 
 public class NoFall extends Module {
     private final ModeValue mode = new ModeValue("Mode", "Packet", "Packet", "Vulcan");
+
+    private final BooleanValue cancel = new BooleanValue("Slient", false);
 
     public NoFall() {
         super("NoFall", "Negates fall damage.", ModuleCategory.PLAYER);
     }
 
     @EventTarget
-    public void onUpdate(MotionUpdateEvent event) {
+    private void onUpdate(MotionUpdateEvent event) {
         if (mc.thePlayer == null || mc.theWorld == null || mc.thePlayer.capabilities.isFlying || mc.thePlayer.capabilities.disableDamage
                 || mc.thePlayer.motionY >= 0.0d || mc.thePlayer.posY <= 0 || event.getEventType() == EventType.POST)
             return;
 
         if (mode.getCurrentMode().equalsIgnoreCase("Packet")) {
-            if (mc.thePlayer.fallDistance > 2F) {
+            if (mc.thePlayer.fallDistance > 2F && isBlockUnder()) {
                 mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(true));
-                mc.thePlayer.fallDistance = 0F;
+                mc.thePlayer.fallDistance *= .1;
             }
         } else if (mode.getCurrentMode().equalsIgnoreCase("Vulcan")) {
             // 随手写的，可以bypass, 如果不重置motionY会爆vClip flag.
@@ -47,6 +51,42 @@ public class NoFall extends Module {
             }
         } else {
             NotificationManager.show(new Notification(NotificationType.WARNING, this.getName(), "Invalid mode: " + mode.getCurrentMode(), 2));
+        }
+    }
+
+    @EventTarget
+    private void onPacket(PacketEvent event) {
+
+        if (event.getPacket() instanceof C03PacketPlayer) {
+            C03PacketPlayer look = (C03PacketPlayer) event.getPacket();
+            if (cancel.getObject())
+                if (look.getRotating() && look.isMoving())
+                    if ((mc.thePlayer.fallDistance > 1.5) && isBlockUnder())
+                        event.setPacket(new C03PacketPlayer.C04PacketPlayerPosition
+                                (look.getPositionX(), look.getPositionY(), look.getPositionZ(), look.isOnGround()));
+
+        }
+
+    }
+
+    public static boolean isBlockUnder() {
+        if (mc.thePlayer.posY < 0.0D) {
+            return false;
+        } else {
+            int off = 0;
+
+            while (true) {
+                if (off >= (int) mc.thePlayer.posY + 2) {
+                    return false;
+                }
+
+                AxisAlignedBB bb = mc.thePlayer.getEntityBoundingBox().offset(0.0D, -off, 0.0D);
+                if (!mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, bb).isEmpty()) {
+                    return true;
+                }
+
+                off += 2;
+            }
         }
     }
 }
