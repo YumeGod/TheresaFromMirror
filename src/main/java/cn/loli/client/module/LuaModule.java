8db9c191560ebd61d836a0b0361cc3015cb27206
诗音澄鸢 镜苑芳依 <dianwangwin@163.com>
2022-03-16 02:01:37 +0800

    added some event listener for this shit

--- src/main/java/cn/loli/client/module/LuaModule.java
+++ src/main/java/cn/loli/client/module/LuaModule.java
@@ -1,9 +1,6 @@
 package cn.loli.client.module;
 
-import cn.loli.client.events.MotionUpdateEvent;
-import cn.loli.client.events.PacketEvent;
-import cn.loli.client.events.TickEvent;
-import cn.loli.client.events.UpdateEvent;
+import cn.loli.client.events.*;
 import com.darkmagician6.eventapi.EventTarget;
 import com.darkmagician6.eventapi.types.EventType;
 import org.luaj.vm2.Globals;
@@ -74,6 +71,122 @@ public class LuaModule extends Module {
     }
 
     @EventTarget
+    private void onRender(RenderEvent event) {
+        try {
+            this.globals.get("on_render").invoke();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @EventTarget
+    private void onRender(Render2DEvent event) {
+        try {
+            this.globals.get("on_render_2d").invoke();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @EventTarget
+    private void onRender(RenderSREvent event) {
+        try {
+            this.globals.get("on_render_sr").invoke();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @EventTarget
+    private void onBlockReach(BlockReachEvent event) {
+        try {
+            this.globals.get("on_block_reach").invoke();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @EventTarget
+    private void onCollision(CollisionEvent event) {
+        try {
+            this.globals.get("on_collision_block").invoke();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @EventTarget
+    private void onMouseOver(MouseOverEvent event) {
+        try {
+            this.globals.get("on_mouse_over").invoke();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @EventTarget
+    private void onMoveFly(MoveFlyEvent event) {
+        try {
+            this.globals.get("on_move_fly").invoke();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @EventTarget
+    private void onJump(JumpYawEvent event) {
+        try {
+            this.globals.get("on_jump_yaw").invoke();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @EventTarget
+    private void onChat(ChatEvent event) {
+        try {
+            this.globals.get("on_chat").invoke();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @EventTarget
+    private void onKey(KeyEvent event) {
+        try {
+            this.globals.get("on_key").invoke();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @EventTarget
+    private void onMove(PlayerMoveEvent event) {
+        try {
+            this.globals.get("on_move").invoke();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @EventTarget
+    private void onAttack(AttackEvent event) {
+        try {
+            this.globals.get("on_attack").invoke();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @EventTarget
+    private void onJump(JumpEvent event) {
+        try {
+            this.globals.get("on_jump").invoke();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
     @Override
