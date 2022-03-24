fca0fb48710c28ff500198e0fa604990535e695c
fakebri <bilibri@protonmail.com>
2022-03-25 01:20:07 +0800

    simple noslow

--- src/main/java/cn/loli/client/module/modules/movement/NoSlowDown.java
+++ src/main/java/cn/loli/client/module/modules/movement/NoSlowDown.java
@@ -6,16 +6,19 @@ import cn.loli.client.module.Module;
 import cn.loli.client.module.ModuleCategory;
 import cn.loli.client.utils.player.PlayerUtils;
 import cn.loli.client.value.BooleanValue;
+import cn.loli.client.value.ModeValue;
 import com.darkmagician6.eventapi.EventTarget;
 import com.darkmagician6.eventapi.types.EventType;
 import net.minecraft.item.ItemBow;
+import net.minecraft.network.play.client.C07PacketPlayerDigging;
 import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
 import net.minecraft.network.play.client.C09PacketHeldItemChange;
 import net.minecraft.network.play.server.S30PacketWindowItems;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 
 public class NoSlowDown extends Module {
-
-    private final BooleanValue vanilla = new BooleanValue("Only Vanilla", false);
+    private final ModeValue mode = new ModeValue("Mode","Vanilla","Vanilla","Packet","Semi-Packet");
     private final BooleanValue itemswitch = new BooleanValue("New", false);
     private final BooleanValue onlyOnMove = new BooleanValue("Only on move", false);
 
@@ -32,8 +35,28 @@ public class NoSlowDown extends Module {
         if (!mc.thePlayer.isUsingItem() || (mc.thePlayer.getHeldItem().getItem() != null && mc.thePlayer.getHeldItem().getItem() instanceof ItemBow) || (onlyOnMove.getObject() && !playerUtils.isMoving2()))
             return;
 
-        if (vanilla.getObject()) return;
+        switch (mode.getCurrentMode()) {
+            case "Packet":
+                doPacketMode(event);
+                break;
+            case "Semi-Packet":
+                if (mc.thePlayer.ticksExisted % 2 != 0) return;
+                doPacketMode(event);
+            case "Vanilla":
+            default:
+                break;
+        }
 
+    }
+
+    @EventTarget
+    public void onPacket(PacketEvent event) {
+        if (event.getPacket() instanceof S30PacketWindowItems)
+            event.setCancelled(mc.thePlayer.isUsingItem());
+
+    }
+
+    private void doPacketMode(MotionUpdateEvent event) {
         if (event.getEventType() == EventType.PRE) {
             final int curSlot = mc.thePlayer.inventory.currentItem;
             final int spoof = curSlot == 0 ? 1 : -1;
@@ -46,13 +69,6 @@ public class NoSlowDown extends Module {
         if (event.getEventType() == EventType.POST) {
             mc.getNetHandler().addToSendQueue(new C08PacketPlayerBlockPlacement(mc.thePlayer.inventory.getCurrentItem()));
         }
-    }
-
-    @EventTarget
-    public void onPacket(PacketEvent event) {
-        if (event.getPacket() instanceof S30PacketWindowItems)
-            event.setCancelled(mc.thePlayer.isUsingItem());
-
     }
 
 }
