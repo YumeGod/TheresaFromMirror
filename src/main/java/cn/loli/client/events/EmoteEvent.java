62d12d92b50e96ae7c73d05d4501ce58fc54eb81
诗音澄鸢 镜苑芳依 <dianwangwin@163.com>
2022-03-25 12:14:17 +0800

    Emote Event

--- src/main/java/cn/loli/client/events/EmoteEvent.java
+++ src/main/java/cn/loli/client/events/EmoteEvent.java
@@ -3,14 +3,17 @@ package cn.loli.client.events;
 import com.darkmagician6.eventapi.events.Event;
 import com.darkmagician6.eventapi.types.EventType;
 import net.minecraft.client.model.ModelBiped;
+import net.minecraft.entity.Entity;
 
 public class EmoteEvent implements Event {
     public ModelBiped biped;
     public EventType type;
+    public Entity entity;
 
-    public EmoteEvent(ModelBiped biped , EventType type) {
+    public EmoteEvent(ModelBiped biped , Entity entity, EventType type) {
         this.biped = biped;
         this.type = type;
+        this.entity = entity;
     }
 
     public ModelBiped getBiped() {
--- src/main/java/cn/loli/client/injection/mixins/MixinModelBiped.java
+++ src/main/java/cn/loli/client/injection/mixins/MixinModelBiped.java
@@ -5,6 +5,7 @@ import cn.loli.client.events.EmoteEvent;
 import com.darkmagician6.eventapi.EventManager;
 import com.darkmagician6.eventapi.types.EventType;
 import net.minecraft.client.model.ModelBiped;
+import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,13 +15,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinModelBiped {
 
     @Inject(method = {"setRotationAngles"}, at = @At(value = "FIELD", target = "Lnet/minecraft/client/model/ModelBiped;heldItemRight:I", ordinal = 0, shift = At.Shift.BEFORE))
-    private void setPreEmote(CallbackInfo ci) {
-        EventManager.call(new EmoteEvent((ModelBiped) (Object) this, EventType.PRE));
+    private void setPreEmote(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn, CallbackInfo ci) {
+        EventManager.call(new EmoteEvent((ModelBiped) (Object) this, entityIn, EventType.PRE));
     }
 
     @Inject(method = {"setRotationAngles"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ModelBiped;copyModelAngles(Lnet/minecraft/client/model/ModelRenderer;Lnet/minecraft/client/model/ModelRenderer;)V", shift = At.Shift.BEFORE))
-    private void setPostEmote(CallbackInfo ci) {
-        EventManager.call(new EmoteEvent((ModelBiped) (Object) this, EventType.POST));
+    private void setPostEmote(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn, CallbackInfo ci) {
+        EventManager.call(new EmoteEvent((ModelBiped) (Object) this, entityIn, EventType.POST));
     }
 
 }
