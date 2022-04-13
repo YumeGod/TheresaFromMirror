a8650e0b9979a2074af7932188b9a87ced12ecfc
SuperSkidder <SuperSkidder@outlook.com>
2022-04-13 11:14:14 +0800

    ClickGui

--- src/main/java/cn/loli/client/gui/clickui/dropdown/panels/components/NumberComponent.java
+++ src/main/java/cn/loli/client/gui/clickui/dropdown/panels/components/NumberComponent.java
@@ -44,20 +44,20 @@ public class NumberComponent extends Component {
         this.x = x;
         this.y = y;
         // Number value
-        RenderUtils.drawRoundRect(x-5, y, x + width, y + 6, 3, theme.option_bg.getRGB());
+        RenderUtils.drawRoundRect(x, y, x + width, y + 6, 3, theme.option_bg.getRGB());
 
         float vX = (((Number) value.getObject()).floatValue() - ((NumberValue<?>) value).getMin().floatValue()) / (((NumberValue<?>) value).getMax().floatValue() - ((NumberValue<?>) value).getMin().floatValue());
         value.clickgui_anim = sizeDrag ? vX * (width) : AnimationUtils.smoothAnimation(value.clickgui_anim, vX * (width), ANIMATION_SPEED, ANIMATION_SCALE);
         //RenderUtils.drawRoundRect(x + width - showValueX + 5, valuesY + 13, x + width - showValueX + 10 + v.clickgui_anim, valuesY + 19, 3, theme.themeColor.getRGB());
 
-        RenderUtils.roundedRect(x-5, y, value.clickgui_anim+5, 6, 3, theme.themeColor.getRGB(), .5f, theme.themeColor.getRGB());
+        RenderUtils.roundedRect(x, y, value.clickgui_anim, 6, 3, theme.themeColor.getRGB(), .5f, theme.themeColor.getRGB());
 
         //RenderUtils.drawRect(x + width - showValueX + 5, valuesY + 13, x + width - 14, valuesY + 19, 0x88ff0000);
 
         DecimalFormat df = new DecimalFormat("#.##");
 
         String bs = df.format(value.getObject());
-        Main.INSTANCE.fontLoaders.get("heiti18").drawString(bs, x + 20 - (Main.INSTANCE.fontLoaders.get("heiti16").getStringWidth(bs)), y, theme.value_number_value.getRGB(), false);
+        Main.INSTANCE.fontLoaders.get("heiti18").drawString(bs, x + vX * (width) / 2 - (Main.INSTANCE.fontLoaders.get("heiti16").getStringWidth(bs)) / 2f, y - 1, theme.value_number_value.getRGB(), false);
 
         // 设置number的值
         if (((NumberValue<?>) value).clickgui_drag && Mouse.isButtonDown(0)) {
