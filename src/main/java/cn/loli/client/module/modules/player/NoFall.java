f389fda7de4a88d57ed491ec906ccbe8f4e6836f
诗音澄鸢 镜苑芳依 <dianwangwin@163.com>
2022-04-15 06:09:50 +0800

    Collision No Fall

--- src/main/java/cn/loli/client/module/modules/player/NoFall.java
+++ src/main/java/cn/loli/client/module/modules/player/NoFall.java
@@ -18,10 +18,12 @@ import net.minecraft.network.play.client.C03PacketPlayer;
 import net.minecraft.util.AxisAlignedBB;
 
 public class NoFall extends Module {
-    private final ModeValue mode = new ModeValue("Mode", "Packet", "Packet", "Vulcan", "Edit");
+    private final ModeValue mode = new ModeValue("Mode", "Packet", "Packet", "Vulcan", "Edit", "Collision");
+
+    private float fallDist, lastTickFallDist;
 
     private final BooleanValue cancel = new BooleanValue("Slient", false);
-    private final BooleanValue isvoid = new BooleanValue("Ignore Void", false);
+    private final BooleanValue isVoid = new BooleanValue("Ignore Void", false);
 
     public NoFall() {
         super("NoFall", "Negates fall damage.", ModuleCategory.PLAYER);
@@ -33,28 +35,60 @@ public class NoFall extends Module {
                 || mc.thePlayer.motionY >= 0.0d || mc.thePlayer.posY <= 0 || event.getEventType() == EventType.POST)
             return;
 
-        if (mode.getCurrentMode().equalsIgnoreCase("Packet")) {
-            if (mc.thePlayer.fallDistance > 2F && (isvoid.getObject() || isBlockUnder())) {
-                mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(true));
-                mc.thePlayer.fallDistance *= .1;
-            }
-        } else if (mode.getCurrentMode().equalsIgnoreCase("Vulcan")) {
-            // 随手写的，可以bypass, 如果不重置motionY会爆vClip flag.
-            if (mc.thePlayer.fallDistance > 2F) {
-                double lastReportedY = ((IAccessorEntityPlayerSP) mc.thePlayer).getLastReportedPosY();
-                if (lastReportedY - Math.floor(lastReportedY) < 0.8F) {
-                    event.setY(Math.floor(lastReportedY));
-                    event.setOnGround(true);
-                    mc.thePlayer.fallDistance = 0F;
-                    mc.thePlayer.motionY = -0.0784000015258789;
+        if (mc.thePlayer.fallDistance == 0)
+            fallDist = 0;
+
+        fallDist += mc.thePlayer.fallDistance - lastTickFallDist;
+        lastTickFallDist = mc.thePlayer.fallDistance;
+
+        switch (mode.getCurrentMode()) {
+            case "Packet":
+                if (fallDist > 2F && (isVoid.getObject() || isBlockUnder())) {
+                    mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(true));
+                    fallDist *= .1;
                 }
-            }
-        } else if (mode.getCurrentMode().equalsIgnoreCase("Edit")) {
-            if (mc.thePlayer.fallDistance > 2F && (isvoid.getObject() || isBlockUnder()))
-                event.setOnGround(true);
-        } else {
-            NotificationManager.show(new Notification(NotificationType.WARNING, this.getName(), "Invalid mode: " + mode.getCurrentMode(), 2));
+                break;
+            case "Vulcan":
+                // 随手写的，可以bypass, 如果不重置motionY会爆vClip flag.
+                if (fallDist > 2F) {
+                    double lastReportedY = ((IAccessorEntityPlayerSP) mc.thePlayer).getLastReportedPosY();
+                    if (lastReportedY - Math.floor(lastReportedY) < 0.8F) {
+                        event.setY(Math.floor(lastReportedY));
+                        event.setOnGround(true);
+                        fallDist = 0F;
+                        mc.thePlayer.motionY = -0.0784000015258789;
+                    }
+                }
+                break;
+            case "Edit":
+                if (fallDist > 2F && (isVoid.getObject() || isBlockUnder()))
+                    event.setOnGround(true);
+                break;
+            case "Collision":
+                if (cancel.getObject()) {
+                    if (fallDist > 3) {
+                        mc.getNetHandler().getNetworkManager().sendPacket(new C03PacketPlayer.C06PacketPlayerPosLook(
+                                (mc.thePlayer.posX + mc.thePlayer.lastTickPosX) / 2,
+                                (mc.thePlayer.posY - (mc.thePlayer.posY % (1 / 64.0))),
+                                (mc.thePlayer.posZ + mc.thePlayer.lastTickPosZ) / 2,
+                                mc.thePlayer.rotationYaw,
+                                mc.thePlayer.rotationPitch,
+                                true), null);
+                        fallDist = 0;
+                    }
+                } else {
+                    if (fallDist > 3) {
+                        mc.thePlayer.motionY = -(mc.thePlayer.posY - (mc.thePlayer.posY - (mc.thePlayer.posY % (1.0 / 64.0))));
+                        event.setOnGround(true);
+                        fallDist = 0;
+                    }
+                }
+                break;
+            default:
+                NotificationManager.show(new Notification(NotificationType.WARNING, this.getName(), "Invalid mode: " + mode.getCurrentMode(), 2));
+                break;
         }
+
     }
 
     @EventTarget
@@ -64,7 +98,7 @@ public class NoFall extends Module {
             C03PacketPlayer look = (C03PacketPlayer) event.getPacket();
             if (cancel.getObject())
                 if (look.getRotating() && look.isMoving())
-                    if ((mc.thePlayer.fallDistance > 1) && isBlockUnder())
+                    if ((fallDist > 1) && isBlockUnder())
                         event.setPacket(new C03PacketPlayer.C04PacketPlayerPosition
                                 (look.getPositionX(), look.getPositionY(), look.getPositionZ(), look.isOnGround()));
 
