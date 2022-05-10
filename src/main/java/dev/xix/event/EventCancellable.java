d1793b0dc20521e7b53f818c18c99bceb78f8753
诗音澄鸢 镜苑芳依 <dianwangwin@163.com>
2022-05-11 03:21:31 +0800

    1

--- src/main/java/dev/xix/event/EventCancellable.java
+++ src/main/java/dev/xix/event/EventCancellable.java
@@ -1,20 +1,16 @@
 package dev.xix.event;
 
-import dev.xix.event.bus.Cancellable;
-
-public abstract class EventCancellable extends Event implements Cancellable  {
+public abstract class EventCancellable extends Event {
 
     private boolean cancelled;
 
     protected EventCancellable() {
     }
 
-    @Override
     public boolean isCancelled() {
         return cancelled;
     }
 
-    @Override
     public void setCancelled(boolean state) {
         cancelled = state;
     }
